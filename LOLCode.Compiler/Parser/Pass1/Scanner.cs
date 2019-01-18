43f0f8d238ae0ef0bd2ab72297dee517629b00b7
Jason Bock <jason@jasonbock.net>
2019-01-18 16:04:19 -0600

    Removed Token duplication

--- LOLCode.Compiler/Parser/Pass1/Scanner.cs
+++ LOLCode.Compiler/Parser/Pass1/Scanner.cs
@@ -1,21 +1,9 @@
-
 using System;
 using System.Collections.Generic;
 using System.IO;
 
 namespace LOLCode.Compiler.Parser.Pass1
 {
-
-	internal class Token
-	{
-		public int kind;    // token kind
-		public int pos;     // token position in the source text (starting at 0)
-		public int col;     // token column (starting at 0)
-		public int line;    // token line (starting at 1)
-		public string val;  // token value
-		public Token next;  // ML 2005-03-11 Tokens are kept in linked list
-	}
-
 	//-----------------------------------------------------------------------------------
 	// Buffer
 	//-----------------------------------------------------------------------------------
---
+++ LOLCode.Compiler/Parser/Token.cs
@@ -0,0 +1,12 @@
+﻿namespace LOLCode.Compiler.Parser
+{
+	internal class Token
+	{
+		public int kind;    // token kind
+		public int pos;     // token position in the source text (starting at 0)
+		public int col;     // token column (starting at 0)
+		public int line;    // token line (starting at 1)
+		public string val;  // token value
+		public Token next;  // ML 2005-03-11 Tokens are kept in linked list
+	}
+}
