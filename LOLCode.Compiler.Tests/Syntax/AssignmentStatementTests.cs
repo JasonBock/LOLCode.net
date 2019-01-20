cd853c7ce68ea52fa750e94c799e417e310d5c1a
Jason Bock <jason@jasonbock.net>
2019-01-19 21:49:18 -0600

    Started working on tests for AssignmentStatement

---
+++ LOLCode.Compiler.Tests/Syntax/AssignmentStatementTests.cs
@@ -0,0 +1,28 @@
+﻿using LOLCode.Compiler.Syntax;
+using NUnit.Framework;
+using System;
+using System.Reflection;
+using System.Reflection.Emit;
+
+namespace LOLCode.Compiler.Tests.Syntax
+{
+	public static class AssignmentStatementTests
+	{
+		[Test]
+		public static void Create()
+		{
+			var assemblyName = new AssemblyName("a");
+			var fileName = $"{assemblyName.Name}.dll";
+			var assemblyBuilder = AppDomain.CurrentDomain.DefineDynamicAssembly(
+				assemblyName, AssemblyBuilderAccess.RunAndSave);
+			var moduleBuilder = assemblyBuilder.DefineDynamicModule(assemblyName.Name, fileName, true);
+			var document = moduleBuilder.DefineDocument(fileName, Guid.Empty, Guid.Empty, Guid.Empty);
+			var pragma = new CodePragma(document, fileName, 0, 0);
+			var statement = new AssignmentStatement(pragma);
+
+			Assert.That(statement.location, Is.SameAs(pragma), nameof(statement.location));
+			Assert.That(statement.lval, Is.Null, nameof(statement.lval));
+			Assert.That(statement.rval, Is.Null, nameof(statement.rval));
+		}
+	}
+}
--- LOLCode.Compiler/Syntax/CodeObject.cs
+++ LOLCode.Compiler/Syntax/CodeObject.cs
@@ -6,8 +6,10 @@ namespace LOLCode.Compiler.Syntax
 {
 	internal abstract class CodeObject
 	{
+		// TODO: This should be readonly
 		public CodePragma location;
 
+		// TODO: This should be protected.
 		public CodeObject(CodePragma loc) => this.location = loc;
 
 		public abstract void Process(LOLMethod lm, CompilerErrorCollection errors, ILGenerator gen);
