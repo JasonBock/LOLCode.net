fa2a6f010f3bbeb17ac0c53c30c03c266a1ebce7
Jason Bock <jason@jasonbock.net>
2019-01-29 22:46:29 -0600

    Update ConditionalStatementTests.cs

--- LOLCode.Compiler.Tests/Syntax/ConditionalStatementTests.cs
+++ LOLCode.Compiler.Tests/Syntax/ConditionalStatementTests.cs
@@ -1,17 +1,56 @@
-﻿using NUnit.Framework;
-using System;
+﻿using LOLCode.Compiler.Syntax;
+using NUnit.Framework;
+using System.CodeDom.Compiler;
 
 namespace LOLCode.Compiler.Tests.Syntax
 {
 	public static class ConditionalStatementTests
 	{
 		[Test]
-		public static void Create() => throw new NotImplementedException();
+		public static void Create()
+		{
+			var (pragma, _, _) = SyntaxEmitGenerators.Create();
+			var statement = new ConditionalStatement(pragma);
+
+			Assert.That(statement.location, Is.SameAs(pragma), nameof(statement.location));
+			Assert.That(statement.condition, Is.Null, nameof(statement.condition));
+			Assert.That(statement.trueStatements, Is.Null, nameof(statement.trueStatements));
+			Assert.That(statement.falseStatements, Is.Null, nameof(statement.falseStatements));
+		}
 
 		[Test]
-		public static void Emit() => throw new NotImplementedException();
+		public static void Emit()
+		{
+			var (pragma, method, ilGenerator) = SyntaxEmitGenerators.Create();
+
+			var statement = new ConditionalStatement(pragma)
+			{
+				condition = new MockExpression(pragma),
+				trueStatements = new MockStatement(pragma),
+				falseStatements = new MockStatement(pragma),
+				ifFalse = ilGenerator.DefineLabel(),
+				statementEnd = ilGenerator.DefineLabel(),
+				location = pragma
+			};
+			statement.Emit(method, ilGenerator);
+			Assert.That(ilGenerator.ILOffset, Is.EqualTo(13), nameof(ilGenerator.ILOffset));
+		}
 
 		[Test]
-		public static void Process() => throw new NotImplementedException();
+		public static void Process()
+		{
+			var (pragma, method, ilGenerator) = SyntaxEmitGenerators.Create();
+			var errors = new CompilerErrorCollection();
+
+			var statement = new ConditionalStatement(pragma)
+			{
+				condition = new MockExpression(pragma),
+				trueStatements = new MockStatement(pragma),
+				falseStatements = new MockStatement(pragma),
+				location = pragma
+			};
+			statement.Process(method, errors, ilGenerator);
+			Assert.That(ilGenerator.ILOffset, Is.EqualTo(6), nameof(ilGenerator.ILOffset));
+		}
 	}
 }
