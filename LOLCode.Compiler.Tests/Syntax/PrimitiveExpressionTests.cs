fdde44eba6f3fdb6fd6f66e2aa60c2e2402babfe
Jason Bock <jason@jasonbock.net>
2019-01-30 11:53:54 -0600

    Update PrimitiveExpressionTests.cs

--- LOLCode.Compiler.Tests/Syntax/PrimitiveExpressionTests.cs
+++ LOLCode.Compiler.Tests/Syntax/PrimitiveExpressionTests.cs
@@ -1,20 +1,55 @@
-﻿using NUnit.Framework;
+﻿using LOLCode.Compiler.Syntax;
+using NUnit.Framework;
 using System;
+using System.CodeDom.Compiler;
 
 namespace LOLCode.Compiler.Tests.Syntax
 {
 	public static class PrimitiveExpressionTests
 	{
 		[Test]
-		public static void Create() => throw new NotImplementedException();
+		public static void Create()
+		{
+			var (pragma, _, _) = SyntaxEmitGenerators.Create();
+			var expression = new PrimitiveExpression(pragma);
+
+			Assert.That(expression.location, Is.SameAs(pragma), nameof(expression.location));
+			Assert.That(expression.value, Is.Null, nameof(expression.value));
+		}
 
 		[Test]
-		public static void CreateWithValue() => throw new NotImplementedException();
+		public static void CreateWithValue()
+		{
+			var value = new object();
+			var (pragma, _, _) = SyntaxEmitGenerators.Create();
+			var expression = new PrimitiveExpression(pragma, value);
+
+			Assert.That(expression.location, Is.SameAs(pragma), nameof(expression.location));
+			Assert.That(expression.value, Is.SameAs(value), nameof(expression.value));
+		}
 
 		[Test]
-		public static void Emit() => throw new NotImplementedException();
+		public static void Emit()
+		{
+			var (pragma, method, ilGenerator) = SyntaxEmitGenerators.Create();
+
+			var value = new object();
+			var expression = new PrimitiveExpression(pragma, value);
+			expression.Emit(method, typeof(object), ilGenerator);
+			Assert.That(ilGenerator.ILOffset, Is.EqualTo(0), nameof(ilGenerator.ILOffset));
+		}
 
 		[Test]
-		public static void Process() => throw new NotImplementedException();
+		public static void Process()
+		{
+			var (pragma, method, ilGenerator) = SyntaxEmitGenerators.Create();
+			var errors = new CompilerErrorCollection();
+
+			var value = 3;
+			var expression = new PrimitiveExpression(pragma, value);
+			expression.Process(method, errors, ilGenerator);
+			Assert.That(ilGenerator.ILOffset, Is.EqualTo(0), nameof(ilGenerator.ILOffset));
+			Assert.That(errors.Count, Is.EqualTo(0), nameof(errors.Count));
+		}
 	}
 }
