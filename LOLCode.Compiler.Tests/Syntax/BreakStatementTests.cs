3300b9a34687fc591b22553e5f55ef5828bfe06e
Jason Bock <jason@jasonbock.net>
2019-01-29 22:20:10 -0600

    Update BreakStatementTests.cs

--- LOLCode.Compiler.Tests/Syntax/BreakStatementTests.cs
+++ LOLCode.Compiler.Tests/Syntax/BreakStatementTests.cs
@@ -1,17 +1,42 @@
-﻿using NUnit.Framework;
-using System;
+﻿using LOLCode.Compiler.Syntax;
+using NUnit.Framework;
+using System.CodeDom.Compiler;
 
 namespace LOLCode.Compiler.Tests.Syntax
 {
 	public static class BreakStatementTests
 	{
 		[Test]
-		public static void Create() => throw new NotImplementedException();
+		public static void Create()
+		{
+			var (pragma, _, _) = SyntaxEmitGenerators.Create();
+			var statement = new BreakStatement(pragma);
+
+			Assert.That(statement.location, Is.SameAs(pragma), nameof(statement.location));
+			Assert.That(statement.label, Is.Null, nameof(statement.label));
+		}
 
 		[Test]
-		public static void Emit() => throw new NotImplementedException();
+		public static void Emit()
+		{
+			var (pragma, method, ilGenerator) = SyntaxEmitGenerators.Create();
+
+			var statement = new BreakStatement(pragma);
+			statement.Emit(method, ilGenerator);
+
+			Assert.That(ilGenerator.ILOffset, Is.EqualTo(2));
+		}
 
 		[Test]
-		public static void Process() => throw new NotImplementedException();
+		public static void Process()
+		{
+			var (pragma, method, ilGenerator) = SyntaxEmitGenerators.Create();
+			var errors = new CompilerErrorCollection();
+
+			var statement = new BreakStatement(pragma);
+			statement.Process(method, errors, ilGenerator);
+
+			Assert.That(ilGenerator.ILOffset, Is.EqualTo(0));
+		}
 	}
 }
