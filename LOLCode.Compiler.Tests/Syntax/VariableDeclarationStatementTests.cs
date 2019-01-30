126263346d122cc7d449559f5ad72e243eabe5c9
Jason Bock <jason@jasonbock.net>
2019-01-30 11:13:18 -0600

    Update VariableDeclarationStatementTests.cs

--- LOLCode.Compiler.Tests/Syntax/VariableDeclarationStatementTests.cs
+++ LOLCode.Compiler.Tests/Syntax/VariableDeclarationStatementTests.cs
@@ -1,17 +1,53 @@
-﻿using NUnit.Framework;
+﻿using LOLCode.Compiler.Symbols;
+using LOLCode.Compiler.Syntax;
+using NUnit.Framework;
 using System;
+using System.CodeDom.Compiler;
 
 namespace LOLCode.Compiler.Tests.Syntax
 {
 	public static class VariableDeclarationStatementTests
 	{
 		[Test]
-		public static void Create() => throw new NotImplementedException();
+		public static void Create()
+		{
+			var (pragma, _, _) = SyntaxEmitGenerators.Create();
+			var statement = new VariableDeclarationStatement(pragma);
+
+			Assert.That(statement.location, Is.SameAs(pragma), nameof(statement.location));
+			Assert.That(statement.var, Is.Null, nameof(statement.var));
+			Assert.That(statement.expression, Is.Null, nameof(statement.expression));
+		}
 
 		[Test]
-		public static void Emit() => throw new NotImplementedException();
+		public static void Emit()
+		{
+			var (pragma, method, ilGenerator) = SyntaxEmitGenerators.Create();
+
+			var statement = new VariableDeclarationStatement(pragma)
+			{
+				var = new LocalRef("a"),
+				expression = new MockExpression(pragma)
+			};
+			statement.Emit(method, ilGenerator);
+			Assert.That(ilGenerator.ILOffset, Is.EqualTo(2), nameof(ilGenerator.ILOffset));
+		}
 
 		[Test]
-		public static void Process() => throw new NotImplementedException();
+		public static void Process()
+		{
+			var (pragma, method, ilGenerator) = SyntaxEmitGenerators.Create();
+			var errors = new CompilerErrorCollection();
+
+			var statement = new VariableDeclarationStatement(pragma)
+			{
+				var = new LocalRef("a"),
+				expression = new MockExpression(pragma)
+			};
+			statement.Process(method, errors, ilGenerator);
+
+			Assert.That(ilGenerator.ILOffset, Is.EqualTo(0), nameof(ilGenerator.ILOffset));
+			Assert.That(errors.Count, Is.EqualTo(0), nameof(errors.Count));
+		}
 	}
 }
